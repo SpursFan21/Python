370f4b6d9f9c10842a77dae2d13409e5e6b07dce
Duncan Turner <duncanfrederic@gmail.com>
2024-01-09 18:56:42 +1300

    recipes

---
+++ Coffee Machine.py
@@ -0,0 +1,43 @@
+logo = """"
+   ______      ________             __  ___           __    _          
+  / ____/___  / __/ __/__  ___     /  |/  /___ ______/ /_  (_)___  ___ 
+ / /   / __ \/ /_/ /_/ _ \/ _ \   / /|_/ / __ `/ ___/ __ \/ / __ \/ _ \
+/ /___/ /_/ / __/ __/  __/  __/  / /  / / /_/ / /__/ / / / / / / /  __/
+\____/\____/_/ /_/  \___/\___/  /_/  /_/\__,_/\___/_/ /_/_/_/ /_/\___/ 
+                                                                       
+"""
+water = 300
+milk = 200
+coffee = 100
+
+recipe = [
+    {
+        "drink": "espresso",
+        "water": 50,
+        "milk": 0,
+        "coffee": 18
+    },
+    {
+        "drink": "latte",
+        "water": 200,
+        "milk": 150,
+        "coffee": 24   
+    },
+    {
+        "drink": "cappuccino",
+        "water": 250,
+        "milk": 100,
+        "coffee": 24         
+    }
+]
+
+def printReport(water, milk, coffee):
+    print(f"There is {water} ml of water")
+    print(f"There is {milk} ml of milk")
+    print(f"There is {coffee} g of coffee")
+
+def checkResources():
+
+def processCoins():
+
+def processPayment():
