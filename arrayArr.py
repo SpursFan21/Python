36cb200ea4cd0c5eeeea95f1e8625e1209043800
SpursFan21 <duncanfrederic@hmail.com>
2024-03-08 11:02:26 +1300

    array take input

--- arrayArr.py
+++ arrayArr.py
@@ -1,17 +1,20 @@
 import array as arr
 
-x = arr.array("d", (23,34,54,34,77))
-print("\n Array List :\n")
+input_string = input("Enter elements of the array separated by spaces: ")
 
+array_data = [float(num) for num in input_string.split()]
+x = arr.array("d", array_data)
+
+print("\nArray List:")
 for i in x:
-    print(i, end = " ")
+    print(i, end=" ")
 print("\n")
 
 lenX = len(x)
 minX = min(x)
 maxX = max(x)
 sumX = sum(x)
-print(f"Length of Array is {lenX}")
-print(f"First element {minX}")
-print(f"Last element {maxX}")
-print(f"Sum of elements in list {sumX}")
+print(f"Length of Array is: {lenX}")
+print(f"First element: {minX}")
+print(f"Last element: {maxX}")
+print(f"Sum of elements in list: {sumX}")
