52e84bc0965d56a60e41836c9b359d1a787259c1
SpursFan21 <duncanfrederic@hmail.com>
2024-03-08 10:32:34 +1300

    remove final comma

--- SeriesLimit.py
+++ SeriesLimit.py
@@ -3,6 +3,10 @@ n = int(input("Enter the series limit: "))
 if n != 0:
     for i in range(n):
         if i % 2 == 0: #we are increasing by 2
-            print(i, end = " ")#The end here allows us to print into a horizontal row
+            if i == n - 2 :# in this positon we alter then end to remove the final comma
+                end = " "
+            else:
+                end = ", "
+            print(i, end = end)#The end here allows us to print into a horizontal row
 else:
     print("\nnot a valid number")
