a611b2ce77981973bb54ecd3c812959ab4038b5e
SpursFan21 <duncanfrederic@hmail.com>
2024-03-14 09:29:57 +1300

    main table

--- ListOfDictionary.py
+++ ListOfDictionary.py
@@ -21,7 +21,7 @@ class Commands:
         else:
             print("No students in the list")
     
-    @staticmethod        
+    @staticmethod
     def DisplayTotalStudents(students):
         totalStudents = len(students)
         print(f"The total number of students in the registry is: {totalStudents}\n")
@@ -47,6 +47,19 @@ registry = StudentRegistry(students)
 
 if __name__ == "__main__":
     print("Welcome to Duncan's List of Dictionary Reader\n")
-    registry.DisplayStudents()
-    Commands.DisplayAvgAge(students)
-    Commands.DisplayTotalStudents(students)
+    while True:
+        try:
+            command = int(input("Enter 1 to view the Registry, Enter 2 to view average age of students, Enter 3 to view total number of enrolled students, or Enter 4 to exit: \n"))
+            if command == 1:
+                registry.DisplayStudents()
+            elif command == 2:
+                Commands.DisplayAvgAge(students)
+            elif command == 3:
+                print("Total number of enrolled students:", len(students))
+            elif command == 4:
+                print("Exiting the program...")
+                break
+            else:
+                print("Incorrect Command Value, Try Again")
+        except ValueError:
+            print("Invalid input! Please enter a valid number.")
