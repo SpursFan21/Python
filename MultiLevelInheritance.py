015e1ffc9e6b89da3b184fd94ee4db82adb4017b
SpursFan21 <duncanfrederic@hmail.com>
2024-03-28 09:49:37 +1300

    doggy

--- MultiLevelInheritance.py
+++ MultiLevelInheritance.py
@@ -1,14 +1,23 @@
 class Animal:
     def speak(self):
         return "Animal Speaks"
+    
+    def sound(self):
+        return "Animal Makes Noise"
 
 class Dog(Animal):
     def barks(self):
         return "Dog Barks"
     
+    def sound(self):
+        return "Dog Makes Noise"
+    
 class Bulldog(Dog):
     def attack(self):
         return "Bulldog attacks!"
+    
+    def sound(self):
+        return super().sound()
     
 if __name__ == "__main__":
     
@@ -17,9 +26,16 @@ if __name__ == "__main__":
     bulldog = Bulldog()
     
     print(animal.speak())
+    print(animal.sound())
+    print("\n")
+    
     print(dog.speak())
     print(dog.barks())
+    print(dog.sound())
+    print("\n")
+    
     print(bulldog.speak())
     print(bulldog.barks())
     print(bulldog.attack())
+    print(bulldog.sound())
     
