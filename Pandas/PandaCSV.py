5a1f598416a23e9b41fac22dd00ee87b57903758
Duncan Turner <duncanfrederic@gmail.com>
2024-03-31 12:14:19 +1300

    data to dict and data to list methods pandas

--- Pandas/PandaCSV.py
+++ Pandas/PandaCSV.py
@@ -15,9 +15,21 @@ class CSVFileManger:
         print(type(data))#check the data type 
         print("\n")
         
+    def convertDataToDictionary():
+        data = pandas.read_csv("./CSV/weather_data.csv")
+        data_dict = data.to_dict()#Converts dataframe to dictionary
+        print("\n")
+        print(data_dict)
+        
+    def convertDataToList():
+        data = pandas.read_csv("./CSV/weather_data.csv")
+        data_list = data["temp"].to_list()#Converts data column to List
+        print("\n")
+        print(data_list)
+        
 if __name__ == "__main__":
     while True:
-        choice = int(input("Enter 1 to read, 2 to view temp column, 3 to check data type and 4 to exit: "))
+        choice = int(input("Enter 1 to read, 2 to view temp column, 3 to check data type, 4 to convert the data to a dictionary,\n 5 to convert data column to list, and 6 to exit: "))
         if choice == 1:
             CSVFileManger.read()
         elif choice == 2:
@@ -25,6 +37,10 @@ if __name__ == "__main__":
         elif choice == 3:
             CSVFileManger.checkDataType()
         elif choice == 4:
+            CSVFileManger.convertDataToDictionary()
+        elif choice == 5:
+            CSVFileManger.convertDataToList()
+        elif choice == 6:
             print("program shutting down.......")
             break
         else:
