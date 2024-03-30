5fcc01d683eaa67bebf0fee95b74ca8939050b67
Duncan Turner <duncanfrederic@gmail.com>
2024-03-31 12:40:01 +1300

    average of list from pandas

--- Pandas/PandaCSV.py
+++ Pandas/PandaCSV.py
@@ -1,6 +1,6 @@
 import pandas
 
-class CSVFileManger:
+class CSVFileManager:
     def read():
         data = pandas.read_csv("./CSV/weather_data.csv")
         print(data)
@@ -26,21 +26,38 @@ class CSVFileManger:
         data_list = data["temp"].to_list()#Converts data column to List
         print("\n")
         print(data_list)
-        
+        print("\n")
+    
+    @staticmethod    
+    def average():
+        data = pandas.read_csv("./CSV/weather_data.csv")
+        data_list = data["temp"].to_list()#Converts data column to List
+        print("\n")
+        print(data_list)
+        print("\n")
+        total = sum(data_list)
+        count = len(data_list)
+        if count == 0:
+            return 0  # Handle the case where the list is empty to avoid division by zero
+        averageTemp = total / count
+        print(f" thet average temp is {averageTemp}")
+    
 if __name__ == "__main__":
     while True:
-        choice = int(input("Enter 1 to read, 2 to view temp column, 3 to check data type, 4 to convert the data to a dictionary,\n 5 to convert data column to list, and 6 to exit: "))
+        choice = int(input("Enter 1 to read, 2 to view temp column, 3 to check data type, 4 to convert the data to a dictionary,\n 5 to convert data column to list, 6 to find the average of the list, and 7 to exit: "))
         if choice == 1:
-            CSVFileManger.read()
+            CSVFileManager.read()
         elif choice == 2:
-            CSVFileManger.getColumn()
+            CSVFileManager.getColumn()
         elif choice == 3:
-            CSVFileManger.checkDataType()
+            CSVFileManager.checkDataType()
         elif choice == 4:
-            CSVFileManger.convertDataToDictionary()
+            CSVFileManager.convertDataToDictionary()
         elif choice == 5:
-            CSVFileManger.convertDataToList()
-        elif choice == 6:
+            CSVFileManager.convertDataToList()
+        elif choice == 6:        
+            CSVFileManager.average()
+        elif choice == 7:
             print("program shutting down.......")
             break
         else:
