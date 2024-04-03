0fada6c988eba1df0b4c37e76b66b2ca98e1ea40
SpursFan21 <duncanfrederic@hmail.com>
2024-04-04 10:35:55 +1300

    file op read

---
+++ File Operations/basic read.py
@@ -0,0 +1,19 @@
+# read method 1
+
+with open("geek.txt", "r") as file:
+    for each in file:
+        print(each)
+        
+#read method 2
+        
+file = open("geek.txt", "r")
+for each in file:
+    print(each)
+file.close()  # It's good practice to close the file after you're done with it.
+
+
+#read method 3
+
+file = open("geek.txt", "r")
+print(file.read(3)) # this only reads a certain amount of characters from the left
+
